3da29a85a20bb308c95fc5c7311c2c8c3b482a2f
kmolcard <kevin.molcard@e.email>
2024-02-02 10:07:26 +0100

    Move Xcode scheme set to PampleJuceMaOS

--- PamplejuceMacOS.cmake
+++ PamplejuceMacOS.cmake
@@ -12,3 +12,27 @@ endif ()
 
 # By default we don't want Xcode schemes to be made for modules, etc
 set(CMAKE_XCODE_GENERATE_SCHEME OFF)
+
+function(set_xcode_scheme target_name)
+    # It tucks the Plugin varieties into a "Targets" folder and generate an Xcode Scheme manually
+    # Xcode scheme generation is turned off globally to limit noise from other targets
+    # The non-hacky way of doing this is via the global PREDEFINED_TARGETS_FOLDER property
+    # However that doesn't seem to be working in Xcode
+    # Not all plugin types (au, vst) available on each build type (win, macos, linux)
+    foreach (target ${FORMATS} "All")
+        if (TARGET ${target_name}_${target})
+            set_target_properties(${target_name}_${target} PROPERTIES
+                # Tuck the actual plugin targets into a folder where they won't bother us
+                FOLDER "Targets"
+                # Let us build the target in Xcode
+                XCODE_GENERATE_SCHEME ON)
+
+            # Set the default executable that Xcode will open on build
+            # Note: you must manually build the AudioPluginHost.xcodeproj in the JUCE subdir
+            if ((NOT target STREQUAL "All") AND (NOT target STREQUAL "Standalone"))
+                set_target_properties(${target_name}_${target} PROPERTIES
+                    XCODE_SCHEME_EXECUTABLE "${CMAKE_CURRENT_SOURCE_DIR}/JUCE/extras/AudioPluginHost/Builds/MacOSX/build/Debug/AudioPluginHost.app")
+            endif ()
+        endif ()
+    endforeach ()
+endfunction()
